478c52c0945ea756c3e2fd6adecefc3d47f04f9e
Francisco Cisneros <gungraveheat123@gmail.com>
2026-07-03 11:35:00 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,12 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      // Maps '@' to the 'src' directory
       '@': path.resolve(__dirname, './src'),
     },
   },
+  base: '/Portafolio-Vue/',
 })
