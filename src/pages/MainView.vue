cf97a3e640e00961e2cd81f18ba72a4debc92b93
Francisco Cisneros <gungraveheat123@gmail.com>
2026-07-12 22:44:24 -0600

    Update LinkedIn link in MainView.vue

--- src/pages/MainView.vue
+++ src/pages/MainView.vue
@@ -62,7 +62,7 @@ onMounted(() => {
                 {{ $t("hero.github") }}
             </a>
 
-            <a href="https://linkedin.com" target="_blank" class="btn bg-[#0967C2] text-white hover:bg-[#0059b3] border-none px-6 py-3 rounded-full flex items-center gap-2 cursor-pointer font-semibold shadow-lg shadow-[#0967C2]/10 transition-transform duration-300">
+            <a href="https://www.linkedin.com/in/francisco-cisneros-9a388241b" target="_blank" class="btn bg-[#0967C2] text-white hover:bg-[#0059b3] border-none px-6 py-3 rounded-full flex items-center gap-2 cursor-pointer font-semibold shadow-lg shadow-[#0967C2]/10 transition-transform duration-300">
                 <svg aria-label="LinkedIn logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
                     <path fill="white" d="M26.111,3H5.889c-1.595,0-2.889,1.293-2.889,2.889V26.111c0,1.595,1.293,2.889,2.889,2.889H26.111c1.595,0,2.889-1.293,2.889-2.889V5.889c0-1.595-1.293-2.889-2.889-2.889ZM10.861,25.389h-3.877V12.87h3.877v12.519Zm-1.957-14.158c-1.267,0-2.293-1.034-2.293-2.31s1.026-2.31,2.293-2.31,2.292,1.034,2.292,2.31-1.026,2.31-2.292,2.31Zm16.485,14.158h-3.858v-6.571c0-1.802-.685-2.809-2.111-2.809-1.551,0-2.362,1.048-2.362,2.809v6.571h-3.718V12.87h3.718v1.686s1.118-2.069,3.775-2.069,4.556,1.621,4.556,4.975v7.926Z" fill-rule="evenodd"></path>
                 </svg>
